dad3d2d88d3d0ab38fa4482100f2cb28de30a58f
Igor <book_fitness@mail.ru>
2022-05-17 21:11:15 +0300

    Prevent adding jsessionid to Location response header when redirecting from POST /room

--- src/main/java/net/scrumtools/webservice/MainController.java
+++ src/main/java/net/scrumtools/webservice/MainController.java
@@ -1,14 +1,22 @@
 package net.scrumtools.webservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping({"/", "/main"})
 public class MainController {
+
+    @Autowired
+    private HttpSession httpSession;
+
     @GetMapping
     public String mainPage() {
+        System.out.println("sessionId " + httpSession.getId() + " started");
         return "index";
     }
 }
